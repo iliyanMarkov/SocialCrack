a65fff194603b7ceb6c5d6cc7fe15be1c36eaf88
Iliyan Markov <ilkomarkov000@gimail.com>
2021-01-15 15:46:01 +0200

    Create/Delete posts with Authentication Check

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -20,71 +20,71 @@ const generateToken = function(user) {
 }
 
 module.exports = {
-  Mutation: {
-    async login(_, { username, password}) {
-        const { errors, valid } = validateLoginInput(username, password)
+	Mutation: {
+    	async login(_, { username, password}) {
+    	    const { errors, valid } = validateLoginInput(username, password)
 
-        if (!valid) {
-            throw new UserInputError('Errors', { errors })
-        }
+    	    if (!valid) {
+    	        throw new UserInputError('Errors', { errors })
+    	    }
 
-        const user = await User.findOne({ username })
+    	    const user = await User.findOne({ username })
 
-        if (!user) {
-            errors.general = 'User not found'
-            throw new UserInputError('User not found', { errors })
-        } 
+    	    if (!user) {
+    	        errors.general = 'User not found'
+    	        throw new UserInputError('User not found', { errors })
+    	    } 
 
-        const match = await bcrypt.compare(password, user.password)
-        if (!match) {
-            errors.general = 'Wrong credentials'
-            throw new UserInputError('Wrong credentials' , { errors })
-        }
+    	    const match = await bcrypt.compare(password, user.password)
+    	    if (!match) {
+    	        errors.general = 'Wrong credentials'
+    	        throw new UserInputError('Wrong credentials' , { errors })
+    	    }
 
-        const token = generateToken(user)
+    	    const token = generateToken(user)
 
-        return {
-            ...user._doc,
-            id: user._id,
-            token,
-        };
-    },
-    async register(
-      _,
-      { registerInput: { username, email, password, confirmPassword } }
-    ) {
-      // Validate user data
-      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
-      if (!valid) {
-        throw new UserInputError('Errors' , { errors })
-      }
-      // TODO: Make sure user doesn't already exist
-      const user = await User.findOne({ username })
-      if (user) {
-        throw new UserInputError("Username is taken", {
-          errors: {
-            username: "This username is taken",
-          },
-        });
-      }
-      //hash password and create an auth token
-      password = await bcrypt.hash(password, 12)
+    	    return {
+    	        ...user._doc,
+    	        id: user._id,
+    	        token,
+    	    };
+    	},
+    	async register(
+    		_,
+    		{ registerInput: { username, email, password, confirmPassword } }
+    	) {
+    	// Validate user data
+    		const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+    			if (!valid) {
+    	    	throw new UserInputError('Errors' , { errors })
+    		}
+    	// TODO: Make sure user doesn't already exist
+    		const user = await User.findOne({ username })
+    		if (user) {
+    			throw new UserInputError("Username is taken", {
+    	    		errors: {
+    	    			username: "This username is taken",
+    	      		},
+    	    	});
+    	  	}
+    	//hash password and create an auth token
+    		password = await bcrypt.hash(password, 12)
 
-      const newUser = new User({
-        email,
-        username,
-        password,
-        createdAt: new Date().toISOString(),
-      });
+    		const newUser = new User({
+    			email,
+    	    	username,
+    	    	password,
+    	    	createdAt: new Date().toISOString(),
+    	  	});
 
-      const res = await newUser.save();
+    	  	const res = await newUser.save();
 
-      const token = generateToken(res)
-      return {
-        ...res._doc,
-        id: res._id,
-        token,
-      };
-    },
-  },
+    	  	const token = generateToken(res)
+    	  	return {
+    	    	...res._doc,
+    	    	id: res._id,
+    	    	token,
+    	  	};
+		},
+  	},
 };
--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({ req })
 })
 
 mongoose
-  .connect( MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB Connected')
-    return server.listen({ port: 5000 })
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`)
-  })
+	.connect( MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('MongoDB Connected')
+		return server.listen({ port: 5000 })
+	})
+	.then((res) => {
+		console.log(`Server running at ${res.url}`)
+	})
